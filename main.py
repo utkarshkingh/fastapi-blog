163f7cba20553d0e29893124c47df71b61c691b6
utkarsh-ajeet-singh <utkarsh-ajeet.singh@siemens.com>
2026-06-28 17:13:40 +0200

    layout template added

--- main.py
+++ main.py
@@ -25,7 +25,12 @@ posts: list[dict] = [
 @app.get("/", include_in_schema=False)
 @app.get("/posts",include_in_schema=False)
 def home(request: Request):
-    return templates.TemplateResponse(request, "home.html",  {"posts": posts})
+    return templates.TemplateResponse(
+        request,
+        "home.html", 
+        {"posts": posts,"title":"Home"}
+                                         
+    )
 
 
 
